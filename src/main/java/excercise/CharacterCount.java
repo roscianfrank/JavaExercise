47bcaa103601d92d8eb83e866f9a2d31dcb17640
Roscian Frank <roscianfrank@gmail.com>
2025-02-11 18:12:31 +0000

    find first and last element of the map

--- src/main/java/excercise/CharacterCount.java
+++ src/main/java/excercise/CharacterCount.java
@@ -3,6 +3,8 @@ package excercise;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CharacterCount {
     public static void main(String[] args) {
@@ -17,8 +19,18 @@ public class CharacterCount {
             countMap.put(c, countMap.getOrDefault(c, 0) + 1);
         }
 
+        Map<Character, Integer> sorted =
+                countMap.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue())
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, HashMap::new));
+
         for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
             System.out.println("Character '" + entry.getKey() + "' occurs " + entry.getValue() + " time(s)");
         }
+
+        System.out.println("least occurrence " + sorted.entrySet().stream().findFirst());
+        System.out.println("Max occurrence " + sorted.entrySet().stream().skip(sorted.size() - 1).findFirst().get());
+
     }
 }
